250e24e4c972eed739077d05c8983abb755dd05f
vpbatista85 <vitor_pbatista@outlook.com>
2023-02-15 15:17:08 -0300

    updating metrics page with check buttons for models

--- pages/4_metrics.py
+++ pages/4_metrics.py
@@ -2,18 +2,28 @@ import streamlit as st
 import utils
 
 
+model_s=[]
 
+with st.sidebar:
+
+    st.write('Comparar os modelos:')
+    if st.checkbox('Top N', value=True, disabled=False):
+        model_s.append('top')
+    if st.checkbox('Item KNN', value=True, disabled=False):
+        model_s.append('itemknn')
+    if st.checkbox('Funk-SVD', value=True, disabled=False):
+        model_s.append('svd')
 
 
 st.title('Métricas de Negócio')
 tab1, tab2, tab3 = st.tabs(["Cobertura","Ranqueamento","Personalização"])
 
 with tab1:
-    utils.plot_report(st.session_state.coverage_report, figsize=(16,10))
+    utils.plot_report(st.session_state.coverage_report, model_s, figsize=(16,10))
 with tab2:
-    utils.plot_report(st.session_state.ranking_report, figsize=(16,10))
+    utils.plot_report(st.session_state.ranking_report, model_s, figsize=(16,10))
 with tab3:
-    utils.plot_report(st.session_state.personalization_report, figsize=(16,10))    
+    utils.plot_report(st.session_state.personalization_report, model_s, figsize=(16,10))    
 
 st.title('Métricas de Acurácia')
 tab4, tab5= st.tabs(["Classificação","Feedback"])
--- utils.py
+++ utils.py
@@ -1141,7 +1141,8 @@ def get_rating_report(df, ranks):
   return rating_report.sort_values(by=['model', 'rank']).reset_index(drop=True)
 
 
-def plot_report(report, figsize=(16,10)):
+def plot_report(report, model_s, figsize=(16,10)):
+  report=report[report['model'].isin(model_s)].copy()
   metrics = report.drop(['model', 'rank'], axis=1).columns
   fig, axes = plt.subplots(nrows=len(metrics), ncols=1, sharex=True, figsize=figsize)
   axes = [axes] if len(metrics) == 1 else axes
