98a410314ef5d4d5acd9a8e9d755a3191d35246d
vpbatista85 <vitor_pbatista@outlook.com>
2023-02-15 13:06:06 -0300

    updating metrics page, plot type display

--- pages/4_metrics.py
+++ pages/4_metrics.py
@@ -6,19 +6,21 @@ import utils
 
 
 st.title('Métricas de Negócio')
-tab1, tab2 = st.tabs(["Cobertura","Personalização e Ranqueamento"])
+tab1, tab2, tab3 = st.tabs(["Cobertura","Ranqueamento","Personalização"])
 
 with tab1:
     utils.plot_report(st.session_state.coverage_report, figsize=(16,10))
 with tab2:
     utils.plot_report(st.session_state.ranking_report, figsize=(16,10))
+with tab3:
+    utils.plot_report(st.session_state.personalization_report, figsize=(16,10))    
 
 st.title('Métricas de Acurácia')
-tab3, tab4= st.tabs(["Classificação","Feedback"])
-
-with tab3:
-    utils.plot_report(st.session_state.classification_report, figsize=(16,10))
+tab4, tab5= st.tabs(["Classificação","Feedback"])
 
 with tab4:
+    utils.plot_report(st.session_state.classification_report, figsize=(16,10))
+
+with tab5:
     utils.plot_report(st.session_state.rating_report, figsize=(16,10))
 
