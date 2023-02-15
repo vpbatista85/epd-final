0c1b636dd9259f08046b3edf1894abbb4a7c86a3
vpbatista85 <vitor_pbatista@outlook.com>
2023-02-15 15:24:20 -0300

    updating metrics page classification metrics plot size

--- pages/4_metrics.py
+++ pages/4_metrics.py
@@ -29,7 +29,7 @@ st.title('Métricas de Acurácia')
 tab4, tab5= st.tabs(["Classificação","Feedback"])
 
 with tab4:
-    utils.plot_report(st.session_state.classification_report, figsize=(16,10))
+    utils.plot_report(st.session_state.classification_report, figsize=(26,10))
 
 with tab5:
     utils.plot_report(st.session_state.rating_report, figsize=(16,10))
